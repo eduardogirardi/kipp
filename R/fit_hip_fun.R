09043b1c579cdf0cca1e3b0507cf647e65a2d492
Eduardo Girardi <eduardo.rgmarques@gmail.com>
2022-12-26 17:23:56 -0300

    v1.0

--- R/fit_hip_fun.R
+++ R/fit_hip_fun.R
@@ -35,7 +35,7 @@ fit_hip <- function(bd, proj_name, path_outputs, estrato){
 
 
   #filtra codigos problematicos
-  rmcod <- c("E", "H", "J", "T", "A", "B")
+  rmcod <- c("E", "H", "J", "A", "B")
   base <- base %>%
     dplyr::filter(is.na(cod1) | cod1 %in% rmcod) %>%
     dplyr::filter(is.na(cod2) | cod2 %in% rmcod) %>%
