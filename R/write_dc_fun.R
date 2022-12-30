14692a65f9cdc0ac3e14eaf8b004db048c8d6a72
Eduardo Girardi <eduardo.rgmarques@gmail.com>
2022-12-30 09:45:35 -0300

    v1.0

--- R/write_dc_fun.R
+++ R/write_dc_fun.R
@@ -19,12 +19,38 @@
 write_dc <- function(bd, path_outputs){
 
   #defini o nome das principais variaveis
-  dc_names <- c("atividade","rf", "talhao", "ciclo", "rotacao", "dt_med", "lider", "auxiliar", "parcela", "tipo",
-                "forma", "hr_ini", "hr_fim", "coordX", "coordY", "lado1", "lado2", "inc1", "inc2", "linha", "arvore",
-                "cap", "alt", "cod1", "cod2", "codQ")
+  dc_names <- c("atividade",
+                "rf",
+                "talhao",
+                "ciclo",
+                "rotacao",
+                "dt_med",
+                "lider",
+                "auxiliar",
+                "parcela",
+                "tipo",
+                "forma",
+                "hr_ini",
+                "hr_fim",
+                "coordX",
+                "coordY",
+                "lado1",
+                "lado2",
+                "inc1",
+                "inc2",
+                "linha",
+                "arvore",
+                "cap",
+                "alt",
+                "cod1",
+                "cod2",
+                "codQ")
 
   bd <- bd %>%
-    dplyr::select(tidyselect::all_of(dc_names))
+    dplyr::select(tidyselect::all_of(dc_names)) %>%
+    dplyr::rename("tal" = "talhao",
+                  "hora_ini" = "hr_ini",
+                  "hora_fim" = "hr_fim")
 
   readr::write_csv2(bd, file.path(path_outputs, "dados_campo_hest.csv"), na = ".")
 
