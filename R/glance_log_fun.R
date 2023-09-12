81908cae0f8410f7c058d4497b7fe208a6d09359
Eduardo Girardi <eduardo.rgmarques@gmail.com>
2023-09-12 07:45:43 -0300

    1.1.4

---
+++ R/glance_log_fun.R
@@ -0,0 +1,34 @@
+#' glance log
+#'
+#' Esta função ainda esta em construcao, por hora acredito que funcione para lm e nlm \cr
+#'
+#'
+#' @param x modelo (lm e nlm)
+#'
+#'
+#' @return um dataframe
+#'
+#' @examples
+#'
+#' glance2(mol)
+#'
+#' @export
+#'
+glance_log <- function(x){
+  temp <- broom::augment(x) %>%
+    mutate(obs = exp(.resid + .fitted),
+           fit = exp(.fitted),
+           resid = obs - fit,
+           residP = (obs - fit)/obs)
+
+
+  syx <- sqrt(sum(temp$resid**2)/(dim(temp)[[1]]-(1+length(x$coefficients)-1)))
+  syxP <- syx/mean(temp$obs)
+
+
+  df <- broom::glance(x) %>%
+    dplyr::mutate(sigma = syx,
+                  sigmaP = syxP,
+                  .after = sigma)
+  return(df)
+}
